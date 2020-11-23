7aee49f883c8de64fbd68a0bd4003f2d1f1ced6e
Igor284 <kochat.lgor@gmail.com>
2020-11-23 15:05:49 +0200

    Desc.

---
+++ WindowsFormsApp3/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    class Controller
+    {
+        public static double input(TextBox t)
+        {
+            double x = 0;
+            if (double.TryParse(t.Text, out x))
+            {
+                return x;
+            }
+            else
+            {
+                t.Text = "input type error, try again";
+            }
+
+            return x;
+        }
+
+        public static double desc(double a, double b, double c)
+        {
+            return (b * b - 4 * a * c);
+        }
+
+
+        public static double x1(double b, double D, double a)
+
+        {
+            return (b * -1) + Math.Sqrt(D) / 2 * a;
+
+        }
+        public static double x2(double b, double D, double a)
+
+        {
+            return (b * -1) - Math.Sqrt(D) / 2 * a;
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+
+
+
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form 
+
+
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            double a = Controller.input(textBox1);
+            double b = Controller.input(textBox2);
+            double c = Controller.input(textBox3);
+
+            double D = Controller.desc(a, b, c);
+            if (D < 0)
+            {
+                MessageBox.Show("Не имеет корней");
+            }
+            else if (D == 0)
+            {
+                MessageBox.Show("Имеет один корень = " + Controller.x1(b, D, a));
+                MessageBox.Show(a + "(x - " + Controller.x1(b, D, a) + ") ** 2");
+            }
+            else
+            {
+                MessageBox.Show(a + "(x - " + Controller.x1(b, D, a) + ")(x - " + Controller.x2(b, D, a) + ")");
+                MessageBox.Show("Имеет два корня. x1 = " + Controller.x1(b, D, a) + "; x2 = " + Controller.x2(b, D, a) + "");
+            }
+        }
+
+    }
+}
